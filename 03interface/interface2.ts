6bc1b72e8c5913d3d1c87f1765a3d7d84013ed38
Minkyu0424 <yng1404@gmail.com>
2023-12-18 00:53:29 +0900

    231217/exercise1-4

--- 03interface/interface2.ts
+++ 03interface/interface2.ts
@@ -1,20 +1,20 @@
-interface Add{
-    (num1:number, num2:number): number; //반환값
+interface Add {
+  (num1: number, num2: number): number; //반환값
 }
 
-const addNum:Add = function(x,y){
-    return x+y;
+const addNum: Add = function (x, y) {
+  return x + y;
+};
+
+add(10, 20);
+
+interface IsAdult {
+  (age: number): boolean;
 }
 
-add(10,20);
+const ageConfirm: IsAdult = (age) => {
+  return age > 19;
+};
+ageConfirm(23);
 
-interface IsAdult{
-    (age:number):boolean;
-}
-
-const ageConfirm:IsAdult = (age) =>{
-    return age > 19;
-}
-ageConfirm(23)
-
-export{};
+export {};
