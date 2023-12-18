b6d3644b69bbb9812b01fe3f296af2ac0aa630f6
Minkyu0424 <yng1404@gmail.com>
2023-12-18 22:10:40 +0900

    231218/챌린지 1일차

---
+++ TS_Challenge/im_explicit.ts
@@ -0,0 +1,21 @@
+let a = "hello";
+a = "bye";
+
+// let b: boolean = "x";
+let b: boolean = false;
+let c = false; //위와 같은 선언
+
+let d = [1, 2, 3];
+let e: number[] = [];
+e.push(1);
+console.log(e);
+//명시적 표현은 최소화하기.
+
+const player ={
+    name:"minkyu"
+}
+// player.hello();
+ 
+const kimchi = {
+    맛있어: true
+}
