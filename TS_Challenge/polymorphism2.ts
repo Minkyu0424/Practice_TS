537ca2e9de2a65624d118ef81a25fd4f4ef7981d
Minkyu0424 <yng1404@gmail.com>
2023-12-28 01:03:19 +0900

    prettier적용

--- TS_Challenge/polymorphism2.ts
+++ TS_Challenge/polymorphism2.ts
@@ -3,30 +3,29 @@ interface SStorage<T> {
 }
 abstract class LocalStorage<T> {
   public storage: SStorage<T> = {};
-  abstract set(key: string, value: T): void 
-  abstract remove(key: string): void 
-  abstract get(key: string):  T | null
-  abstract clear(key: string): void 
+  abstract set(key: string, value: T): void;
+  abstract remove(key: string): void;
+  abstract get(key: string): T | null;
+  abstract clear(key: string): void;
 }
 
-class LocalStorageMini  extends LocalStorage<number>{
-    set(key:string, value:number){
-        this.storage[key]= value
+class LocalStorageMini extends LocalStorage<number> {
+  set(key: string, value: number) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): number | null {
+    if (this.storage[key] !== undefined) {
+      return null;
+    } else {
+      return this.storage[key];
     }
-    remove(key:string){
-        delete this.storage[key]
-    }
-    get(key: string): number | null {
-        if(this.storage[key] !== undefined){
-            return null
-        }else{
-            return this.storage[key]
-        }
-    }
-    clear(key: string): void {
-        this.storage={}
-    }
+  }
+  clear(key: string): void {
+    this.storage = {};
+  }
 }
-
 
 export {};
